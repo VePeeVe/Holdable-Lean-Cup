628c9af05b362608b98d9270be02b5f02defc243
ve <105993367+VePeeVe@users.noreply.github.com>
2022-06-28 18:52:24 -0600

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace lean
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ve.gorillatag.lean";
+        public const string Name = "lean";
+        public const string Version = "1.0.0";
+    }
+}
